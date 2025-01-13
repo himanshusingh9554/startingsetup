ec4b10e20a5c5187f7eac694e55664a4fb86a1a6
himanshusingh9554 <himanshusingh9554@gmail.com>
2025-01-13 21:19:25 +0530

    add sequilizer

--- util/database.js
+++ util/database.js
@@ -1,9 +1,8 @@
-const mysql = require('mysql12');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-compelete',
-    password: 'root'
+const sequelize = new Sequelize('node-compelete', 'root', 'root', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
-module.exports = pool.promise();
+
+module.exports = sequelize;
